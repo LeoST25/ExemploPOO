05025009252cbb922672fbddd4d8f23124edf25d
LeoST25 <matheusleonardo224@gmail.com>
2024-05-30 03:12:30 -0300

    Comentando os objetos criados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using ExemploPOO.Models;
 
-
+//Objeto Conta Corrente
 ContaCorrente c1 = new ContaCorrente(123, 1000);
 
 c1.ExibirSaldo();
@@ -19,30 +19,9 @@ c1.ExibirSaldo();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Objeto Pessoa
 /*Pessoa p1 = new Pessoa();
 p1.Nome = "Leonardo";
 p1.Idade = 20;
 
 p1.Apresentar();*/
-
- 
