f259429389567ea328d0a166829ead3bb26dac74
LeoST25 <matheusleonardo224@gmail.com>
2024-05-29 21:08:15 -0300

    Add project files.

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models {
+    public class ContaCorrente {
+        public ContaCorrente(int numeroConta, decimal saldoInicial) {
+            NumeroConta = numeroConta;
+            Saldo = saldoInicial;
+        }
+
+        public int NumeroConta { get; set; }
+
+        private decimal Saldo;
+         
+        public void Sacar(decimal valor) {
+            if (Saldo >= valor)
+            {
+            Saldo -= valor;
+            Console.WriteLine($"Saque realizado com sucesso");                
+            }
+            else
+            {
+                Console.WriteLine("valor desejado é maior que o saldo disponível");
+            }
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Seu saldo disponível é: " + Saldo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ExemploPOO.Models;
+
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Pessoa p1 = new Pessoa();
+p1.Nome = "Leonardo";
+p1.Idade = 20;
+
+p1.Apresentar();*/
+
+ 
